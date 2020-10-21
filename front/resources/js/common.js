c91fbac78a09730d2a1c5518b9ca20e04aaaed01
park-geun <rmstn133@gmail.com>
2020-10-21 18:49:18 +0900

    문의하기 팝업 검색결과 페이지 제작

--- front/resources/js/common.js
+++ front/resources/js/common.js
@@ -1,3 +1,82 @@
+window.onload= function(){
+    $('nav>ul>li').hover(function () {
+        var idx = $(this).index() + 1;
+        console.log(idx);
+        $('#gnb-' + idx).show();//nav hover gnb open
+        $('#gnb-' + idx).siblings().hide();
+        $('.gnb').show();
+        $('#nav-'+idx).addClass('active');//nav text hover
+        $('#nav-'+idx).siblings().removeClass('active');
+    }, function () {})
+    
+     //gnb 삭제
+     $('.gnb').on('mouseleave', function(){
+        $(this).hide();
+    })
+
+    $(function(){
+    $('.tab-title li').click(function(){
+        var i = $(this).index();
+
+        $('.tab-title li').removeClass('active');
+        $(this).addClass('active');
+        $('.tab-content > div').removeClass('active').eq(i).addClass('active');
+    });
+    //전체메뉴 2뎁스 펼치기
+	$('.mobile_menu .bottom .left > li').click(function(){
+		var index = $(this).index();
+		console.log('asd');
+		$('.mobile_menu .bottom .left > li').removeClass('on');
+		$(this).addClass('on');
+		$('.mobile_menu .bottom .right > li').removeClass('on').eq(index).addClass('on');
+	});
+	//전체메뉴 3뎁스 펼치기
+	$('.mobile_menu .bottom .right > li > span').click(function(){
+		var depth_height = $(this).next().find('ul').height();
+
+		if($(this).hasClass('on') === true){
+			$(this).removeClass('on');
+			$(this).next().css({'height':'0'});
+		}else{
+			$(this).addClass('on');
+			$(this).next().css({'height':depth_height});
+		}
+    });
+    //toggle_list
+	$('.toggle_list .list .title').click(function(){
+
+		if($(this).parents('.list').hasClass('on') === true){
+			$(this).parents('.list').removeClass('on');
+		}else{
+			$(this).parents('.list').addClass('on');
+		}
+	});
+});
+
+function openMobileMenu() {
+    $('.mobile_menu').css({'right':'0'});
+}
+
+function closeMoblieBtn() {
+    $('.mobile_menu').css({'right':'-100%'});
+}
+
+///공지사항 다음탭 이동 스크립트
+function goNoticeView(){
+    $('#footer_notice_list').modal('hide');
+    $('#footer_notice_view').modal('show');
+
+}
+
+//family site open/close
+function openFamilySite(){
+    $('.famliy-site').show();
+    console.log('open')
+}
+function closeFamilySite(){
+    $('.famliy-site').hide();
+}
+}
 $(document).ready(function () {
 
     $('.include-header').load('../layout/header.html');
