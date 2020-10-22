6a195c1747aeaf68ff3c6c85b10e318a04627b42
park-geun <rmstn133@gmail.com>
2020-10-22 18:57:59 +0900

    피드백 수정

--- front/resources/js/common.js
+++ front/resources/js/common.js
@@ -1,81 +1,92 @@
-window.onload= function(){
+window.onload = function () {
+
+
     $('nav>ul>li').hover(function () {
         var idx = $(this).index() + 1;
         console.log(idx);
-        $('#gnb-' + idx).show();//nav hover gnb open
+        $('#gnb-' + idx).show(); //nav hover gnb open
         $('#gnb-' + idx).siblings().hide();
         $('.gnb').show();
-        $('#nav-'+idx).addClass('active');//nav text hover
-        $('#nav-'+idx).siblings().removeClass('active');
+        $('#nav-' + idx).addClass('active'); //nav text hover
+        $('#nav-' + idx).siblings().removeClass('active');
     }, function () {})
-    
-     //gnb 삭제
-     $('.gnb').on('mouseleave', function(){
+
+    //gnb 삭제
+    $('.gnb').on('mouseleave', function () {
         $(this).hide();
     })
 
-    $(function(){
-    $('.tab-title li').click(function(){
-        var i = $(this).index();
+    $(function () {
+        $('.tab-title li').click(function () {
+            var i = $(this).index();
 
-        $('.tab-title li').removeClass('active');
-        $(this).addClass('active');
-        $('.tab-content > div').removeClass('active').eq(i).addClass('active');
+            $('.tab-title li').removeClass('active');
+            $(this).addClass('active');
+            $('.tab-content > div').removeClass('active').eq(i).addClass('active');
+        });
+        //전체메뉴 2뎁스 펼치기
+        $('.mobile_menu .bottom .left > li').click(function () {
+            var index = $(this).index();
+            console.log('asd');
+            $('.mobile_menu .bottom .left > li').removeClass('on');
+            $(this).addClass('on');
+            $('.mobile_menu .bottom .right > li').removeClass('on').eq(index).addClass('on');
+        });
+        //전체메뉴 3뎁스 펼치기
+        $('.mobile_menu .bottom .right > li > span').click(function () {
+            var depth_height = $(this).next().find('ul').height();
+
+            if ($(this).hasClass('on') === true) {
+                $(this).removeClass('on');
+                $(this).next().css({
+                    'height': '0'
+                });
+            } else {
+                $(this).addClass('on');
+                $(this).next().css({
+                    'height': depth_height
+                });
+            }
+        });
+        //toggle_list
+        $('.toggle_list .list .title').click(function () {
+
+            if ($(this).parents('.list').hasClass('on') === true) {
+                $(this).parents('.list').removeClass('on');
+            } else {
+                $(this).parents('.list').addClass('on');
+            }
+        });
     });
-    //전체메뉴 2뎁스 펼치기
-	$('.mobile_menu .bottom .left > li').click(function(){
-		var index = $(this).index();
-		console.log('asd');
-		$('.mobile_menu .bottom .left > li').removeClass('on');
-		$(this).addClass('on');
-		$('.mobile_menu .bottom .right > li').removeClass('on').eq(index).addClass('on');
-	});
-	//전체메뉴 3뎁스 펼치기
-	$('.mobile_menu .bottom .right > li > span').click(function(){
-		var depth_height = $(this).next().find('ul').height();
-
-		if($(this).hasClass('on') === true){
-			$(this).removeClass('on');
-			$(this).next().css({'height':'0'});
-		}else{
-			$(this).addClass('on');
-			$(this).next().css({'height':depth_height});
-		}
-    });
-    //toggle_list
-	$('.toggle_list .list .title').click(function(){
-
-		if($(this).parents('.list').hasClass('on') === true){
-			$(this).parents('.list').removeClass('on');
-		}else{
-			$(this).parents('.list').addClass('on');
-		}
-	});
-});
+}
 
 function openMobileMenu() {
-    $('.mobile_menu').css({'right':'0'});
+    $('.mobile_menu').css({
+        'right': '0'
+    });
 }
 
 function closeMoblieBtn() {
-    $('.mobile_menu').css({'right':'-100%'});
+    $('.mobile_menu').css({
+        'right': '-100%'
+    });
 }
 
 ///공지사항 다음탭 이동 스크립트
-function goNoticeView(){
+function goNoticeView() {
     $('#footer_notice_list').modal('hide');
     $('#footer_notice_view').modal('show');
 
 }
 
 //family site open/close
-function openFamilySite(){
+function openFamilySite() {
     $('.famliy-site').show();
     console.log('open')
 }
-function closeFamilySite(){
+
+function closeFamilySite() {
     $('.famliy-site').hide();
-}
 }
 $(document).ready(function () {
 
@@ -94,18 +105,18 @@ $(document).ready(function () {
             prevEl: '.swiper-button-prev',
         },
         loop: true,
-        autoplay:{
-            delay:3000,
+        autoplay: {
+            delay: 3000,
         }
     });
 
-    $('.play-btn').on("click",function(){
-        if( $(this).hasClass('active') ){ 
+    $('.play-btn').on("click", function () {
+        if ($(this).hasClass('active')) {
             console.log('2');
             $(this).removeClass('active');
             mySwiper.autoplay.start();
-            
-        }else{
+
+        } else {
             $(this).addClass('active');
             mySwiper.autoplay.stop();
             console.log('1');
@@ -113,60 +124,53 @@ $(document).ready(function () {
 
     })
 
-
-   
-
-
-
     ///글래스 hover
-    $('.glass-list').hover(function(){
-        var idx = $(this).index()+1;
-        var glassImg = $('#glass_img_'+idx);
+    $('.swiper-slide').hover(function () {
+        var idx = $(this).index() + 1;
+        var glassImg = $('#glass_img_' + idx);
+        console.log(idx);
+        $(this).children().addClass('active');
+        glassImg.attr('style', 'background-image:url(../../resources/images/phone-' + idx + '.png)');
 
-        $(this).addClass('active');
-        glassImg.attr('style','background-image:url(../../resources/images/phone-'+idx+'.png)');
-        
-    },function(){
-        var idx = $(this).index()+1;
-        var glassImg = $('#glass_img_'+idx);
+    }, function () {
+        var idx = $(this).index() + 1;
+        var glassImg = $('#glass_img_' + idx);
 
-        $(this).removeClass('active');
-        glassImg.attr('style',' background-image:url(../../resources/images/product-'+idx+'.png)');
-
-    })
-
-
-    $('.glass-list.camera').hover(function(){
-        var idx = $(this).index()+1;
-        var glassImg = $('#glass_img_'+idx);
-
-        $(this).addClass('active');
-        glassImg.attr('style','background-image:url(../../resources/images/phone-'+idx+'.png)');
-        
-    },function(){
-        var idx = $(this).index()+1;
-        var glassImg = $('#glass_img_'+idx);
-
-        $(this).removeClass('active');
-        glassImg.attr('style','background-image:url(../../resources/images/window-glass-'+idx+'.jpg)');
+        $(this).children().removeClass('active');
+        glassImg.attr('style', ' background-image:url(../../resources/images/product-' + idx + '.png)');
 
     })
 
 
-  
-    $( '.top-scroll' ).click( function() {
-        $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+    $('.glass-list.camera').hover(function () {
+        var idx = $(this).index() + 1;
+        var glassImg = $('#glass_img_' + idx);
+
+        $(this).addClass('active');
+        glassImg.attr('style', 'background-image:url(../../resources/images/phone-' + idx + '.png)');
+
+    }, function () {
+        var idx = $(this).index() + 1;
+        var glassImg = $('#glass_img_' + idx);
+
+        $(this).removeClass('active');
+        glassImg.attr('style', 'background-image:url(../../resources/images/window-glass-' + idx + '.jpg)');
+
+    })
+
+
+
+    $('.top-scroll').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
         return false;
-    } );
-    
-   $('.form-control').click(function(){
-        
+    });
+
+    $('.form-control').click(function () {
+
     })
 })
-
-
-
-
 
 function openSearch() {
     $('.search-bar').show();
@@ -186,7 +190,10 @@ function closeBtn() {
     $('.gnb').hide();
 }
 
-function toggleBtn(){
+function toggleBtn() {
     $('.toggle-wrap').toggleClass('active');
 }
- 
+
+
+
+
